cbabba1c65fce8e3418574e232979b46c46e9a26
Zak Nesler <zaknesler@gmail.com>
2018-09-05 15:47:46 -0400

    Add authentication testing helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Sign into the application as a user.
+     *
+     * @param  \App\User $user
+     * @param  array  $overrides
+     * @return \App\User
+     */
+    protected function authenticate($user = null, $overrides = [])
+    {
+        $this->be($user ?: $user = factory(User::class)->create($overrides));
+
+        return $user;
+    }
 }
